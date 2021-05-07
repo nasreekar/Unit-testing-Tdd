0f9531d52cbc590a66c89a8f35458401cdab0477
Abhijith Sreekar <abhijith.sreekar@infra.tigerspike.com>
2021-05-08 00:28:36 +0530

    #Exercise2 - Soln - Write test cases for StringDuplicator Class

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,34 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class StringDuplicatorTest {
 
+    StringDuplicator SUT;
+
+    @Before
+    public void setup(){
+        SUT = new StringDuplicator();
+    }
+
+    @Test
+    public void duplicator_emptyString_emptyStringReturned(){
+        String result = SUT.duplicate("");
+        assertEquals(result, "");
+    }
+
+    @Test
+    public void duplicator_singleCharacterString_singleCharacterIsDoubled(){
+        String result = SUT.duplicate("a");
+        assertEquals(result, "aa");
+    }
+
+    @Test
+    public void duplicator_multiCharacterString_inputIsDoubled(){
+        String result = SUT.duplicate("abhi");
+        assertEquals(result, "abhiabhi");
+    }
 }
